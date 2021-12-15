dd7a3fb1bb18d33e6b379df6ee663185dba13b1e
pauliescanlon <paul.scanlon@gatsbyjs.com>
2021-12-15 14:09:32 -0500

    feat: add collection route

--- src/pages/{nycPhoto.slug}.js
+++ src/pages/{nycPhoto.slug}.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
 import LeftArrow from '../components/left-arrow';
-import RightArrow from '../components/right-arrow';
 
 import Heart from '../components/heart';
 import User from '../components/user';
@@ -15,8 +14,7 @@ const Template = ({
       urls: { regular },
       likes
     }
-  },
-  pageContext
+  }
 }) => {
   return (
     <main>
@@ -28,7 +26,7 @@ const Template = ({
         <h1 className="font-black text-3xl ">{description}</h1>
         <p className="text-sm font-normal">{alt_description}</p>
         <div className="grid grid-cols-auto-1fr gap-4 items-end text-sm">
-          <a href={portfolio_url} target="_blank" rel="noreferrer" className="grid grid-cols-auto-1fr gap-1 items-center hover:text-purple-200 transition">
+          <a href={portfolio_url} target="_blank" rel="noreferrer" className="cursor-pointer grid grid-cols-auto-1fr gap-1 items-center hover:text-purple-200 transition">
             <User />
             {name}
           </a>
@@ -36,24 +34,6 @@ const Template = ({
             <Heart />
             {likes}
           </div>
-        </div>
-      </div>
-      <div className="absolute top-0 left-0 w-full h-screen z-10">
-        <div className="flex justify-between items-center w-full h-screen">
-          {pageContext.prev ? (
-            <Link className="bg-brand-primary text-white p-4 font-bold capitalize" to={`/${pageContext.prev.slug}`}>
-              <LeftArrow />
-            </Link>
-          ) : (
-            <div />
-          )}
-          {pageContext.next ? (
-            <Link className="bg-brand-primary text-white p-4 font-bold capitalize" to={`/${pageContext.next.slug}`}>
-              <RightArrow />
-            </Link>
-          ) : (
-            <div />
-          )}
         </div>
       </div>
       <div className="absolute top-0 left-0 w-full h-screen bg-cover bg-center z-1">
